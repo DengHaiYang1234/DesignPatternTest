c1032a28174a209be711ceea7c7dfd28a6b5d3f4
denghaiyang <429078038@qq.com>
2019-12-17 19:36:35 +0800

    2019.12.17
    控制台创建三种模式

---
+++ DesignPatternTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatternTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
