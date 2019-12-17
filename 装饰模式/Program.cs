05a166f1cb3110a9e47ad2298e2cfb32195078d1
denghaiyang <429078038@qq.com>
2019-12-17 19:40:13 +0800

    2019.12.17
    更新

--- 装饰模式/Program.cs
+++ 装饰模式/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-namespace DesignPatternTest
+namespace 装饰模式
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
         }
     }
 }
